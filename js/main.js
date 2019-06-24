10e920a7233a733249c703e6f8d47e0e75d12d07
Nadezhda <nadezhda@legeart.pro>
2019-06-24 07:01:36 +0300

    Добавила функцию для случайных чисел координат

--- js/main.js
+++ js/main.js
@@ -2,14 +2,22 @@
 
 var pins = [];
 var offerTypes = ["palace", "flat", "house", "bungalo"];
+var coordinateMap = document.querySelector('.map').offsetWidth;
+
+console.log('.map');
 
 function getRandomItem(arr) {
   var index = Math.floor(Math.random() * arr.length); // Случайный индекс массива
   return arr[index];
 }
+
 function getRandomNumber(min, max) {
   // на основе предыдущей функции нужно сделать  вычисление со 130 до 630
   // и подобием сделать с объектом location внутри цикла
+  var rand = min + Math.random() * (max - min + 1);
+  rand = Math.round(rand);
+
+  return rand;
 }
 
 for (var i = 0; i < 8; i++) {
@@ -21,13 +29,19 @@ for (var i = 0; i < 8; i++) {
       type: getRandomItem(offerTypes)
     },
     location: {
-      x: 200,
-      y: getRandomNumber(130,630)
+      x: getRandomNumber(0, 1200),
+      y: getRandomNumber(130, 630)
     }
   };
 
   pins.push(pin);
-  // console.log(pins);
 }
 
-// console.log(pins);
+// var renderMapPins = function(mapPins) {
+//   var pinMapping = document.querySelectorAll('.map__pin');
+// }
+
+// Это временное решение,
+// этот класс переключает карту из неактивного состояния в активное.
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
