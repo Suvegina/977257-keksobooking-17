362c155a7296f0a1fcb2d21439de204b099987cd
nadin <nadezhda@legeart.pro>
2019-06-25 18:06:24 +0300

    Добавила комментарий который нужно будет сгенерировать в цикле

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 
 var pins = [];
 var offerTypes = ['palace', 'flat', 'house', 'bungalo'];
-// var coordinateMap = document.querySelector('.map').offsetWidth;
 
+// Проверка размера ширины окна (класса)
+// var coordinateMap = document.querySelector('.map').offsetWidth;
 // console.log('.map');
 
 function getRandomItem(arr) {
@@ -36,6 +37,21 @@ for (var i = 0; i < 8; i++) {
 
   pins.push(pin);
 }
+
+/*
+
+<button class="map__pin map__pin--main" style="left: 570px; top: 375px;">
+    <img src="img/muffin-red.svg" width="40" height="44" draggable="false" alt="Метка объявления">
+    <svg viewBox="0 0 70 70" width="156" height="156" aria-label="Метка для поиска жилья">
+      <defs>
+        <path d="M35,35m-23,0a23,23 0 1,1 46,0a23,23 0 1,1 -46,0" id="tophalf" />
+      </defs>
+      <ellipse cx="35" cy="35" rx="35" ry="35" fill="rgba(255, 86, 53, 0.7)" />
+      <text><textPath xlink:href="#tophalf" startOffset="0">Поставь меня куда-нибудь</textPath></text>
+    </svg>
+  </button>  
+  
+*/
 
 // var renderMapPins = function(mapPins) {
 //   var pinMapping = document.querySelectorAll('.map__pin');
