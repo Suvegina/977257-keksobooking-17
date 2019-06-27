5224a6fbb62aaa037547f7a9337aba5296c39ead
Nadezhda <nadezhda@legeart.pro>
2019-06-27 21:25:03 +0300

    Исправила отображение пинов

--- js/main.js
+++ js/main.js
@@ -10,6 +10,13 @@ var MAX_WIDTH = mapPin.offsetWidth;
 var minRAndomHeight = 130;
 var maxRAndomHeight = 630;
 
+// смещение пинов относительно нужной метки
+var PIN_POSITION_X = 20;
+var PIN_POSITION_Y = 62;
+
+// размеры метки объявления
+var ELEM_PIN_WIDTH = 40;
+var ELEM_PIN_HEIGTH = 40;
 
 
 function getRandomItem(arr) {
@@ -29,7 +36,7 @@ function getRandomNumber(min, max) {
 for (var i = 0; i < 8; i++) {
   var pin = {
     author: {
-      avatar: 'img/avatars/user0' + (i+1) + '.png'
+      avatar: 'img/avatars/user0' + (i + 1) + '.png'
     },
     offer: {
       type: getRandomItem(offerTypes)
@@ -45,31 +52,28 @@ for (var i = 0; i < 8; i++) {
   pins.push(pin);
 }
 
-function makeButton (pin) {
+function makeButton(pin) {
   var element = document.createElement('button');
-  element.classList.add('map__pin', 'map__pin--main');
-  element.style.left = pin.location.x - 20 + 'px';
-  // Нужно убрать свойство border на пине
-  // Так же нужно переопределить цвет элемента :before 
-  // и перерасчитать его положение на карте
-  element.style.top = pin.location.y - 66 + 'px';
+  element.classList.add('map__pin');
+  element.style.left = pin.location.x - PIN_POSITION_X + 'px';
+  element.style.top = pin.location.y - PIN_POSITION_Y + 'px';
   element.appendChild(makeImg(pin.author.avatar));
 
   return element;
 };
 
-function makeImg (src) {
+function makeImg(src) {
   var element = document.createElement('img');
   element.src = src;
   element.alt = 'Метка объявления';
-  element.width = 40;
-  element.height = 44;
+  element.width = ELEM_PIN_WIDTH;
+  element.height = ELEM_PIN_HEIGTH;
   element.draggable = false;
   return element;
-};
+}
 
 /*
-
+Вспомогательный код, который подобию него мы выводим в функциях и задействуем их в циклах для решения 3-й задачи
 <button class="map__pin map__pin--main" style="left: 570px; top: 375px;">
     <img src="img/muffin-red.svg" width="40" height="44" draggable="false" alt="Метка объявления">
     <svg viewBox="0 0 70 70" width="156" height="156" aria-label="Метка для поиска жилья">
