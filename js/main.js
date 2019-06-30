e30e7fb4111d2784f043dcbb1ce80a960a21e477
Nadezhda <nadezhda@legeart.pro>
2019-06-30 18:11:43 +0300

    Сделала закрытие заглушки главного пина и показ остальных пинов

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var offerTypes = ['palace', 'flat', 'house', 'bungalo'];
 var mapPin = document.querySelector('.map__pins');
 var MIN_WIDTH = 0;
 // Проверка размера ширины окна (класса)
-var MAX_WIDTH = mapPin.offsetWidth;
+// var MAX_WIDTH = mapPin.offsetWidth;
 // Мин и макс. положение location по Y в цикле
 var minRAndomHeight = 130;
 var maxRAndomHeight = 630;
@@ -14,84 +14,63 @@ var maxRAndomHeight = 630;
 var PIN_POSITION_X = 20;
 var PIN_POSITION_Y = 62;
 
-// размеры метки объявления
-var ELEM_PIN_WIDTH = 40;
-var ELEM_PIN_HEIGTH = 40;
-
-
+ // Находим случайный индекс массива
+ // Для рандомного подбора параметров 'offerTypes'
 function getRandomItem(arr) {
-  var index = Math.floor(Math.random() * arr.length); // Случайный индекс массива
+  var index = Math.floor(Math.random() * arr.length);
   return arr[index];
 }
 
+//Находим случайное число для координат
 function getRandomNumber(min, max) {
-  // на основе предыдущей функции нужно сделать  вычисление со 130 до 630
-  // и подобием сделать с объектом location внутри цикла
   var rand = min + Math.random() * (max - min + 1);
   rand = Math.round(rand);
 
   return rand;
 }
 
-for (var i = 0; i < 8; i++) {
-  var pin = {
+var pinTemplate = document.querySelector('#pin').content.querySelector(".map__pin");
+
+function makeButton(pin) {
+  var element = pinTemplate.cloneNode(true);
+  element.style.left = pin.location.x - PIN_POSITION_X + 'px';
+  element.style.top = pin.location.y - PIN_POSITION_Y + 'px';
+  element.querySelector('img').src = pin.author.avatar;
+
+  return element;
+}
+
+var generatePin = function(index) {
+  var newPin = {
     author: {
-      avatar: 'img/avatars/user0' + (i + 1) + '.png'
+      avatar: 'img/avatars/user0' + (index + 1) + '.png'
     },
     offer: {
       type: getRandomItem(offerTypes)
     },
     location: {
-      x: getRandomNumber(MIN_WIDTH, MAX_WIDTH),
+      x: getRandomNumber(MIN_WIDTH, mapPin.offsetWidth),
       y: getRandomNumber(minRAndomHeight, maxRAndomHeight)
     }
-  };
+  }
+  return newPin;
+};
 
-  mapPin.appendChild(makeButton(pin));
-  // console.log(pin);
-  pins.push(pin);
-}
+var renderButton = function () {
+  for (var i = 0; i < 8; i++) {
+    var pin = generatePin(i);
 
-function makeButton() {
-  var element = document.createElement('button');
-  element.classList.add('map__pin');
-  element.style.left = pin.location.x - PIN_POSITION_X + 'px';
-  element.style.top = pin.location.y - PIN_POSITION_Y + 'px';
-  element.appendChild(makeImg(pin.author.avatar));
+    mapPin.appendChild(makeButton(pin));
+    pins.push(pin);
+  }
+};
 
-  return element;
-}
-
-function makeImg(src) {
-  var element = document.createElement('img');
-  element.src = src;
-  element.alt = 'Метка объявления';
-  element.width = ELEM_PIN_WIDTH;
-  element.height = ELEM_PIN_HEIGTH;
-  element.draggable = false;
-  return element;
-}
-
-/*
-Вспомогательный код, который подобию него мы выводим в функциях и задействуем их в циклах для решения 3-й задачи
-<button class="map__pin map__pin--main" style="left: 570px; top: 375px;">
-    <img src="img/muffin-red.svg" width="40" height="44" draggable="false" alt="Метка объявления">
-    <svg viewBox="0 0 70 70" width="156" height="156" aria-label="Метка для поиска жилья">
-      <defs>
-        <path d="M35,35m-23,0a23,23 0 1,1 46,0a23,23 0 1,1 -46,0" id="tophalf" />
-      </defs>
-      <ellipse cx="35" cy="35" rx="35" ry="35" fill="rgba(255, 86, 53, 0.7)" />
-      <text><textPath xlink:href="#tophalf" startOffset="0">Поставь меня куда-нибудь</textPath></text>
-    </svg>
-  </button>
-
-*/
-
-// var renderMapPins = function(mapPins) {
-//   var pinMapping = document.querySelectorAll('.map__pin');
-// }
-
-// Это временное решение,
-// этот класс переключает карту из неактивного состояния в активное.
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var mapClickHandler =  function() {
+  map.classList.remove('map--faded');
+  renderButton();
+  map.removeEventListener('click', mapClickHandler);
+};
+
+map.addEventListener('click', mapClickHandler);
+
