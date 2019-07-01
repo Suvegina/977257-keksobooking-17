02954c09d15cca079bac52312a311efcef4e8605
Nadezhda <nadezhda@legeart.pro>
2019-07-01 22:20:41 +0300

    Исправила мелкие недочеты в коде. Добавила комментарии для начала работы над заданием

--- js/main.js
+++ js/main.js
@@ -14,14 +14,14 @@ var maxRAndomHeight = 630;
 var PIN_POSITION_X = 20;
 var PIN_POSITION_Y = 62;
 
- // Находим случайный индекс массива
- // Для рандомного подбора параметров 'offerTypes'
+// Находим случайный индекс массива
+// Для рандомного подбора параметров 'offerTypes'
 function getRandomItem(arr) {
   var index = Math.floor(Math.random() * arr.length);
   return arr[index];
 }
 
-//Находим случайное число для координат
+// Находим случайное число для координат
 function getRandomNumber(min, max) {
   var rand = min + Math.random() * (max - min + 1);
   rand = Math.round(rand);
@@ -29,7 +29,7 @@ function getRandomNumber(min, max) {
   return rand;
 }
 
-var pinTemplate = document.querySelector('#pin').content.querySelector(".map__pin");
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 function makeButton(pin) {
   var element = pinTemplate.cloneNode(true);
@@ -40,7 +40,7 @@ function makeButton(pin) {
   return element;
 }
 
-var generatePin = function(index) {
+var generatePin = function (index) {
   var newPin = {
     author: {
       avatar: 'img/avatars/user0' + (index + 1) + '.png'
@@ -52,10 +52,11 @@ var generatePin = function(index) {
       x: getRandomNumber(MIN_WIDTH, mapPin.offsetWidth),
       y: getRandomNumber(minRAndomHeight, maxRAndomHeight)
     }
-  }
+  };
   return newPin;
 };
 
+// функция циклической генерации пинов
 var renderButton = function () {
   for (var i = 0; i < 8; i++) {
     var pin = generatePin(i);
@@ -65,7 +66,9 @@ var renderButton = function () {
   }
 };
 
-var map = document.querySelector('.map');
+var map = document.querySelector ('.map');
+
+// событие при клике на главную метку пина
 var mapClickHandler =  function() {
   map.classList.remove('map--faded');
   renderButton();
@@ -74,3 +77,12 @@ var mapClickHandler =  function() {
 
 map.addEventListener('click', mapClickHandler);
 
+// добавляю всем полям не активное состояние, а именно:
+
+
+// сама форма заполнения информации об объявлении;
+// Все <input> и <select> формы .ad-form заблокированы с помощью атрибута disabled, добавленного на них или на их родительские блоки fieldset;
+// Форма с фильтрами .map__filters заблокирована так же, как и форма .ad-form;
+// Единственное доступное действие в неактивном состоянии — перетаскивание метки
+// .map__pin--main, являющейся контролом указания адреса объявления.
+// Первое перемещение метки переводит страницу в активное состояние.
