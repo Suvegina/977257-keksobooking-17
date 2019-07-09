35fdbfee4472b791a556d5d1b0f0368909e91105
nadin <nadezhda@legeart.pro>
2019-07-09 18:18:55 +0300

    Переместила код обратно в основной файл js кода (код не рабочий)

--- js/main.js
+++ js/main.js
@@ -86,6 +86,76 @@ var renderButton = function () {
 
 form.action = 'https://js.dump.academy/keksobooking';
 
+
+// перемещение метки, события на главной метке.
+
+(function () {
+
+  var map = document.querySelector('.map');
+  var currentPinHandler = map.querySelector('.map__pin--main');
+  // var currentPin = map.querySelector('.map__pin--main');
+
+  currentPinHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };    
+
+    var dragged = false;
+
+    var currentPinMouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+      
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+    
+    // При каждом движении мыши нам нужно обновлять
+    // смещение относительно первоначальной точки, чтобы диалог
+    // смещался на необходимую величину.
+      map.style.top = (map.offsetTop - shift.y) + 'px';
+      map.style.left = (map.offsetLeft - shift.x) + 'px';
+    };
+
+    var currentPinMouseUpHandler = function(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', currentPinMouseMoveHandler);
+      document.removeEventListener('mouseup', currentPinMouseUpHandler);
+      
+      if (dragged) {
+        var preventDefaultClickHandler = function (evt) {
+          evt.preventDefault();
+          currentPinHandler.removeEventListener('click', preventDefaultClickHandler);
+          currentPinHandler.addEventListener('click', preventDefaultClickHandler)
+
+          // перемещаю сюда события ранее находящихся при событии клика по главной метке
+          map.classList.remove('map--faded');
+          renderButton();
+          form.classList.remove('ad-form--disabled');
+          disableFormControl();
+          disableFiltersControl();
+          currentPinHandler.removeEventListener('mousedown', currentPinHandler);
+          currentPinHandler.addEventListener('click', currentPinHandler);
+        };
+      }
+    };
+
+    document.addEventListener('mousemove', currentPinMouseMoveHandler);
+    document.addEventListener('mouseup', currentPinMouseUpHandler);
+  });
+  // currentPin.addEventListener('mousedown', currentPinMouseDownHandler);
+});
+
+
 // событие при клике на главную метку пина
 var getCoordinatePin = function () {
   var x = currentPin.style.left.replace('px', '');
--- js/moving-current-pin.js
+++ js/moving-current-pin.js
@@ -1,72 +1,71 @@
-// перемещение метки, события на главной метке.
+// // перемещение метки, события на главной метке.
 
-(function () {
+// (function () {
 
-  var map = document.querySelector('.map');
-  var currentPinHandler = map.querySelector('.map__pin--main');
-  // var currentPin = map.querySelector('.map__pin--main');
+//   var map = document.querySelector('.map');
+//   var currentPinHandler = map.querySelector('.map__pin--main');
+//   // var currentPin = map.querySelector('.map__pin--main');
 
-  currentPinHandler.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
+//   currentPinHandler.addEventListener('mousedown', function (evt) {
+//     evt.preventDefault();
 
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };    
+//     var startCoords = {
+//       x: evt.clientX,
+//       y: evt.clientY
+//     };    
 
-    var dragged = false;
+//     var dragged = false;
 
-    var currentPinMouseMoveHandler = function (moveEvt) {
-      moveEvt.preventDefault();
+//     var currentPinMouseMoveHandler = function (moveEvt) {
+//       moveEvt.preventDefault();
 
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
+//       var shift = {
+//         x: startCoords.x - moveEvt.clientX,
+//         y: startCoords.y - moveEvt.clientY
+//       };
       
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
+//       startCoords = {
+//         x: moveEvt.clientX,
+//         y: moveEvt.clientY
+//       };
     
-    // При каждом движении мыши нам нужно обновлять
-    // смещение относительно первоначальной точки, чтобы диалог
-    // смещался на необходимую величину.
-      map.style.top = (map.offsetTop - shift.y) + 'px';
-      map.style.left = (map.offsetLeft - shift.x) + 'px';
-    };
+//     // При каждом движении мыши нам нужно обновлять
+//     // смещение относительно первоначальной точки, чтобы диалог
+//     // смещался на необходимую величину.
+//       map.style.top = (map.offsetTop - shift.y) + 'px';
+//       map.style.left = (map.offsetLeft - shift.x) + 'px';
+//     };
 
-    var currentPinMouseUpHandler = function(upEvt) {
-      upEvt.preventDefault();
+//     var currentPinMouseUpHandler = function(upEvt) {
+//       upEvt.preventDefault();
 
-      document.removeEventListener('mousemove', currentPinMouseMoveHandler);
-      document.removeEventListener('mouseup', currentPinMouseUpHandler);
+//       document.removeEventListener('mousemove', currentPinMouseMoveHandler);
+//       document.removeEventListener('mouseup', currentPinMouseUpHandler);
       
-      if (dragged) {
-        var preventDefaultClickHandler = function (evt) {
-          evt.preventDefault();
-          currentPinHandler.removeEventListener('click', preventDefaultClickHandler);
-          currentPinHandler.addEventListener('click', preventDefaultClickHandler)
+//       if (dragged) {
+//         var preventDefaultClickHandler = function (evt) {
+//           evt.preventDefault();
+//           currentPinHandler.removeEventListener('click', preventDefaultClickHandler);
+//           currentPinHandler.addEventListener('click', preventDefaultClickHandler)
 
-          // перемещаю сюда события ранее находящихся при событии клика по главной метке
-          map.classList.remove('map--faded');
-          renderButton();
-          form.classList.remove('ad-form--disabled');
-          disableFormControl();
-          disableFiltersControl();
-          currentPin.removeEventListener('mousedown', currentPinMouseDownHandler);
+//           // перемещаю сюда события ранее находящихся при событии клика по главной метке
+//           map.classList.remove('map--faded');
+//           renderButton();
+//           form.classList.remove('ad-form--disabled');
+//           disableFormControl();
+//           disableFiltersControl();
+//           currentPinHandler.removeEventListener('mousedown', currentPinHandler);
+//           currentPinHandler.addEventListener('click', currentPinHandler);
+//         };
+//       }
+//     };
 
-          currentPinHandler.addEventListener('click', currentPinMouseDownHandler)
-        }
-      }
-    };
+//     document.addEventListener('mousemove', currentPinMouseMoveHandler);
+//     document.addEventListener('mouseup', currentPinMouseUpHandler);
+//   });
 
-    document.addEventListener('mousemove', currentPinMouseMoveHandler);
-    document.addEventListener('mouseup', currentPinMouseUpHandler);
-  });
-
-  // currentPin.addEventListener('mousedown', currentPinMouseDownHandler);
-});
+//   // currentPin.addEventListener('mousedown', currentPinMouseDownHandler);
+// });
 
 // --------------------------------------------------------------------------------
 
@@ -127,7 +126,6 @@
 
 //   currentPin.addEventListener('mousedown', currentPinMouseDownHandler);
 // });
-  
 //   // // событие при клике на главную метку пина
 //   // var currentPinClickHandler = function () {
 //   //   map.classList.remove('map--faded');
@@ -137,5 +135,4 @@
 //   //   disableFiltersControl();
 //   //   currentPin.removeEventListener('click', currentPinClickHandler);
 //   // };
-  
 //   // currentPin.addEventListener('click', currentPinClickHandler);
