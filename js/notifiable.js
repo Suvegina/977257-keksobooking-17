8611236d5a47392f66925742ab05832cdcdf8228
Nadezhda <nadezhda@legeart.pro>
2019-07-16 08:22:13 +0300

    Создан новый файл по отрисовке сообщений (при ошибке / и при успешной отправке данных) - пока что сырой вариант. Нужно дорабатывать!

---
+++ js/notifiable.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// В этом файле определяем выводы всплывающих окно
+// при успешной отправке данных и при возникновении ошибки
+
+(function () {
+
+  var successTemplate = document.querySelector('#success').content.querySelector('main');
+  var errorTemplate = document.querySelector('#error').content.querySelector('main');
+
+  // var successHandler = function(successMessage) {
+  //   // var node = document.createElement('div');
+  //   var node = successTemplate.cloneNode(true);
+  //   node = document.createElement('div');
+  //   node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+  //   node.style.position = 'absolute';
+  //   node.style.left = 0;
+  //   node.style.right = 0;
+  //   node.style.fontSize = '30px';
+
+  //   node.textContent = successMessage;
+  //   document.body.insertAdjacentElement('afterbegin', node);
+  // };
+
+  // var errorHandler = function (errorMessage) {
+  //   // var node = document.createElement('div');
+  //   var node = errorTemplate.cloneNode(true);
+  //   node = document.createElement('div');
+  //   node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+  //   node.style.position = 'absolute';
+  //   node.style.left = 0;
+  //   node.style.right = 0;
+  //   node.style.fontSize = '30px';
+
+  //   node.textContent = errorMessage;
+  //   document.body.insertAdjacentElement('afterbegin', node);
+  // };
+
+  // window.notifiable = {
+  //   successHandler = successHandler,
+  //   errorHandler = errorHandler
+  // }
+
+  var notifiableHandler = function (fromTemplate, Message) {
+    // var node = document.createElement('div');
+    var node = fromTemplate.cloneNode(true);
+    node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = Message;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.notifiable = {
+    notifiableHandler = notifiableHandler
+  }
+
+})();
