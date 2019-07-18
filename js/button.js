5761bbdd93ebb27410fa91f959118c0c26c65f1b
Nadezhda <nadezhda@legeart.pro>
2019-07-18 09:31:06 +0300

    Ненужный файл

--- js/button.js
+++ js/button.js
@@ -1,1 +1,19 @@
-button.js
+// button.js - отрисовка кнопки + навешивание события на кнопку
+
+'use strict';
+
+(function () {
+
+
+  var successTemplate = document.querySelector('#success').content.querySelector('main');
+  var errorTemplate = document.querySelector('#error').content.querySelector('main');
+
+  window.createButton = function(name, clickHandler) {
+    var button = document.createElement('button');
+
+    button.addEventListener('click', clickHandler);
+    button.textContent = name;
+
+    return button;
+  };
+})();
