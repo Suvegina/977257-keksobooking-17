73f51ea401632257e70ee929b8de36982b8615fd
Nadezhda <nadezhda@legeart.pro>
2019-07-19 06:44:16 +0300

    Вернула упрощенный вариант загрузки данных через условие if () else

--- js/load.js
+++ js/load.js
@@ -10,28 +10,10 @@
 
     // задаю функцию запроса для отображения состояния (ответа) сервера
     var onLoadRequest = function () {
-      var error;
-      switch (xhr.status) {
-        case 200:
-          onSuccess(xhr.response);
-          break;
-        case 400:
-          error = 'Bad request';
-          break;
-        case 401:
-          error = 'Access denied';
-          break;
-        case 404:
-          error = 'Not found';
-          break;
-        // case 504:
-        //   error = 'Time out';
-        //   break;
-        default:
-          error = 'Status' + xhr.status + ' ' + xhr.statusText;
-      }
-      if (error) {
-        onError(error);
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError(xhr.status);
       }
     };
 
