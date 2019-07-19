7757a53e08202f58fa98aac00e210ce5bbced3ad
Nadezhda <nadezhda@legeart.pro>
2019-07-19 06:23:44 +0300

    Исправила возможность ошибок запроса с сервера (с помощью switch - конструктора)

--- js/load.js
+++ js/load.js
@@ -2,24 +2,40 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/keksobooking/data';
 
   window.load = function (onSuccess, onError) {
+    var URL = 'https://js.dump.academy/keksobooking/data';
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа' + xhr.status + '' + xhr.statusText);
+    // задаю функцию запроса для отображения состояния (ответа) сервера
+    var onLoadRequest = function () {
+      var error;
+      switch (xhr.status) {
+        case 200:
+          onSuccess(xhr.response);
+          break;
+        case 400:
+          error = 'Bad request';
+          break;
+        case 401:
+          error = 'Access denied';
+          break;
+        case 404:
+          error = 'Not found';
+          break;
+        // case 504:
+        //   error = 'Time out';
+        //   break;
+        default:
+          error = 'Status' + xhr.status + ' ' + xhr.statusText;
       }
-    });
+      if (error) {
+        onError(error);
+      }
+    };
 
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
+    xhr.addEventListener('load', onLoadRequest);
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
