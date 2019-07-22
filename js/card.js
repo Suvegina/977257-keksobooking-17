263a7172ddf971931fd7c6feb6ad0f36bb2c9abd
Nadezhda <nadezhda@legeart.pro>
2019-07-22 09:00:14 +0300

    Вывела заголовок объявления, адрес и тип в карточку пина

--- js/card.js
+++ js/card.js
@@ -1,5 +1,28 @@
 'use strict'; // card.js — модуль, который отвечает за создание карточки объявлений;
 
-// (function () {
-//   // body ...
-// }) ();
+(function () {
+  var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+  var PIN_PRICE = {
+
+  };
+
+  var PIN_TYPES = {
+    flat: 'Квартира',
+    bungalo: 'Бунгало',
+    house: 'Дом',
+    palace: 'Дворец'
+  };
+
+  var renderCard = function (pin) {
+    var card = cardTemplate.cloneNode(true);
+    card.querySelector('.popup__title').textContent = pin.offer.title;
+    card.querySelector('.popup__text--address').textContent = pin.offer.address;
+    // ... price
+    card.querySelector('.popup__type').textContent = PIN_TYPES[pin.offer.type];
+    // popup__photos - выводить через цикл клонируя img (используя cloneNode) и appendChild после этого.
+    document.querySelector('.map').appendChild(card);
+  }
+  window.card = {
+    render: renderCard
+  }
+}) ();
--- js/pin.js
+++ js/pin.js
@@ -32,6 +32,7 @@
     // вызываю функци с синхронизацией адреса (положение главного пина)
     window.load(function (pinsData) {
       window.pin.allPins = pinsData;
+      window.card.render(window.pin.allPins[0]);
       var newPins = window.filter.filterPins(window.pin.allPins);
       renderPins(newPins);
     });
@@ -55,7 +56,6 @@
       pin.remove();
     });
   };
-
 
   window.pin = {
     allPins: [],
