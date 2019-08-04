35409a91caa8df373a5ca4b5db7e4fab8f9f5669
Nadezhda <nadezhda@legeart.pro>
2019-08-04 17:44:47 +0300

    Исправления по критерию Б1 - Добавила удаление отрисованной карточки при изменении фильтров (ТЗ: 5.9)

--- js/filter.js
+++ js/filter.js
@@ -83,6 +83,7 @@
   // создаю callback функцию с функцией debounce(), в которой помещаю все действия связанные с отрисовкой пинов по выбору фильтрам
   var filterChangeHandler = window.debounce(function () {
     var pins = window.filterPins(window.pin.allPins);
+    window.card.delete();
     window.pin.removePins();
     window.pin.renderPins(pins);
   });
